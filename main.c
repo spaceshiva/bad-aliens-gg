9fb553ca5d89ef794bbbec6456024c7cecaa97df
Ricardo Zanini <ricardozanini@gmail.com>
2017-02-28 17:49:36 -0300

    refactoring, #1 playable

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include "SMSlib.h"
+#include "main.h"
 #include "assets/assets.h"
 
 SMS_EMBED_SEGA_ROM_HEADER(9999,0);
@@ -18,10 +19,6 @@ SMS_EMBED_SDSC_HEADER_AUTO_DATE(1, 0, "chains\\2017", "Bad Aliens GG", "a little
 #define GG_Y  3
 #define GG_AREA 20
 
-#define ST_TITLE 0
-#define ST_MAIN 1
-#define ST_GAMEOVER 2
-
 #define START_LIVES 3
 
 #define INIT_P_POSX (GG_X * 8) + 80
@@ -31,15 +28,46 @@ SMS_EMBED_SDSC_HEADER_AUTO_DATE(1, 0, "chains\\2017", "Bad Aliens GG", "a little
 #define MIN_X  (GG_X * 8)
 #define MAX_XY  (GG_X * 8) + 152
 
+#define NUMBER_OF_DIGITS 16
+
 unsigned char gameState;
 unsigned char playerLives;
-unsigned char score;
+unsigned int score;
 unsigned char level;
 unsigned int playerPosition;
 // 0: x, 1: y, 2: dead?
 unsigned int bulletBehavior[3];
 // 0: X, 1: Y, 2: dead?
 unsigned int enemyBehavior[3];
+
+//====================================
+// Utils
+//====================================
+
+// ref.:http://stackoverflow.com/questions/18691677/how-to-put-an-integer-to-an-array-of-digits
+// implementação super simples (mas que funciona)
+// retorna o array ao contrário. necessário exibir em ordem reversa
+void numberToArray(char *buf, unsigned int number) {
+  unsigned int i = 0;
+  while(number != 0) {
+    buf[i] = number % 10;
+    number /= 10;
+    i++;
+  }
+}
+
+//====================================
+// Load Assets
+//====================================
+
+// GG display 160 x 144
+void loadTitleAssets(void) {
+  SMS_loadPSGaidencompressedTiles(title_scene__tiles__psgcompr, BG_TILES);
+  SMS_loadSTMcompressedTileMapArea(GG_X, GG_Y, title_scene__tilemap__stmcompr, GG_AREA);
+  GG_loadBGPalette(default__palette__bin);
+  SMS_loadPSGaidencompressedTiles(press_start__tiles__psgcompr, PS_TILES);
+  GG_setBGPaletteColor(4, RGB(3,3,3));
+}
 
 void loadMainAssets(void) {
   SMS_loadPSGaidencompressedTiles(main_scene__tiles__psgcompr, BG_TILES);
@@ -50,16 +78,9 @@ void loadMainAssets(void) {
   GG_loadSpritePalette(default__palette__bin);
 }
 
-// ref.: http://stackoverflow.com/questions/14564813/how-to-convert-integer-to-character-array-using-c
-void numberToArray(char *buf, unsigned long number) {
-  unsigned int n = log10f(number) + 1;
-  unsigned int i;
-  // no dynamic alloc. :(
-  // ref.: http://stackoverflow.com/questions/17354905/sdcc-and-malloc-allocating-much-less-memory-than-is-available
-  for (i = 0; i < n; ++i, number /= 10) {
-    buf[i] = number % 10;
-  }
-}
+//====================================
+// Variables update
+//====================================
 
 void updateScore(unsigned long points) {
   unsigned char x;
@@ -94,26 +115,20 @@ void updateLives(unsigned char lives) {
   if(lives > 3) {
     return;
   }
-  for(x = 0; x < lives; x++) {
+  for(x = 0; x < 3; x++) {
     SMS_setNextTileatXY(GG_X+16+x,GG_Y+1);
-    pnt = &numbers__tilemap__bin[20];
+    if(lives >= x + 1) {
+      pnt = &numbers__tilemap__bin[20];
+    } else {
+      pnt = &numbers__tilemap__bin[18];
+    }
     SMS_setTile(*pnt+++SCORE_TILES);
   }
 }
 
-void movePlayer(void) {
-  unsigned int ks = SMS_getKeysStatus();
-  if((ks & PORT_A_KEY_LEFT) && (playerPosition > MIN_X)) {
-    playerPosition -= 2;
-  } else if((ks & PORT_A_KEY_RIGHT) && (playerPosition < MAX_XY)) {
-    playerPosition += 2;
-  }
-}
-
-void drawPlayer(void) {
-  SMS_addSprite(playerPosition, INIT_P_POSY, SHIP_TILE);
-}
-
+//====================================
+// Behavior
+//====================================
 int randEnemyXPos(void) {
   unsigned int pos = 0;
   if(playerPosition >= MIN_X && playerPosition <= MIN_X + 16) {
@@ -130,24 +145,45 @@ int randEnemyXPos(void) {
   return pos;
 }
 
+unsigned char checkCollision(unsigned int va[2][2], unsigned int vb[2][2]) {
+  if(va[1][0] < vb[0][0] || va[0][0] > vb[1][0]) return 0;
+  if(va[1][1] < vb[0][1] || va[0][1] > vb[1][1]) return 0;
+
+  return 1;
+}
+
 unsigned char checkBulletEnemyCollision() {
   unsigned int e[2][2];
   unsigned int b[2][2];
 
-  e[0][0] = bulletBehavior[0]+3; //min x
-  e[0][1] = bulletBehavior[1];  //min y
-  e[1][0] = bulletBehavior[0]+4; //max x
+  e[0][0] = bulletBehavior[0]+5; //min x
+  e[0][1] = bulletBehavior[1]+4;  //min y
+  e[1][0] = bulletBehavior[0]+6; //max x
   e[1][1] = bulletBehavior[1]+8; //max y
 
-  b[0][0] = enemyBehavior[0];
-  b[0][1] = enemyBehavior[1];
-  b[1][0] = enemyBehavior[0]+8;
-  b[1][1] = enemyBehavior[1]-8;
+  b[0][0] = enemyBehavior[0]+2; //min x // +2 pixels para diminuir o box
+  b[0][1] = enemyBehavior[1]; // min y
+  b[1][0] = enemyBehavior[0]+8; // max x // -1 pixel para diminuir o box
+  b[1][1] = enemyBehavior[1]+8; //max y
 
-  if(e[1][0] < b[0][0] || e[0][0] > b[1][0]) return 0;
-  if(e[1][1] < b[0][1] || e[0][1] > b[1][1]) return 0;
+  return checkCollision(e, b);
+}
 
-  return 1;
+unsigned char checkEnemyShipCollision() {
+  unsigned int e[2][2];
+  unsigned int s[2][2];
+
+  s[0][0] = playerPosition; //min x
+  s[0][1] = INIT_P_POSY;  //min y (y é fixo)
+  s[1][0] = playerPosition+8; //max x
+  s[1][1] = INIT_P_POSY+8; //max y
+
+  e[0][0] = enemyBehavior[0]+2; //min x // +2 pixels para diminuir o box
+  e[0][1] = enemyBehavior[1]; // min y
+  e[1][0] = enemyBehavior[0]+8; // max x
+  e[1][1] = enemyBehavior[1]+8; //max y
+
+  return checkCollision(e, s);
 }
 
 void checkEnemyOverlapsScreeen(void) {
@@ -156,12 +192,61 @@ void checkEnemyOverlapsScreeen(void) {
   }
 }
 
-void checkBulletCollsion(void) {
+void checkBulletOverlapsScreen(void) {
   if(bulletBehavior[1] <= (GG_Y * 8) + 16) {
     bulletBehavior[2] = 1;
-  } else if (checkBulletEnemyCollision() == 1) {
-    bulletBehavior[2] = 1;
-    enemyBehavior[2] = 1;
+  }
+}
+
+void checkBulletBehavior(void) {
+  checkBulletOverlapsScreen();
+  if(bulletBehavior[2] == 0) {
+    if(checkBulletEnemyCollision() == 1) {
+      bulletBehavior[2] = 1;
+      enemyBehavior[2] = 1;
+      score += 10;
+      updateScore(score);
+    }
+  }
+}
+
+void checkEnemyBehavior() {
+  checkEnemyOverlapsScreeen();
+  if(enemyBehavior[2] == 0) {
+    if(checkEnemyShipCollision() == 1) {
+      updateLives(--playerLives);
+      enemyBehavior[2] = 1;
+      playerPosition = INIT_P_POSX;
+    }
+  }
+}
+
+void checkBehavior(void) {
+   checkEnemyBehavior();
+   checkBulletBehavior();
+   if(playerLives == 0) {
+     gameState = ST_GAMEOVER;
+   }
+}
+//====================================
+// Moving
+//====================================
+void moveEnemy(void) {
+  if(enemyBehavior[2] == 1) {
+    enemyBehavior[2] = 0;
+    enemyBehavior[1] = INIT_E_POSY;
+    enemyBehavior[0] = randEnemyXPos();
+  } else {
+    ++enemyBehavior[1];
+  }
+}
+
+void movePlayer(void) {
+  unsigned int ks = SMS_getKeysStatus();
+  if((ks & PORT_A_KEY_LEFT) && (playerPosition > MIN_X)) {
+    playerPosition -= 2;
+  } else if((ks & PORT_A_KEY_RIGHT) && (playerPosition < MAX_XY)) {
+    playerPosition += 2;
   }
 }
 
@@ -179,21 +264,11 @@ void moveBullet(void) {
     --bulletBehavior[1];
   }
 }
-
-void drawBullet(void) {
-  if(bulletBehavior[2] == 0) {
-    SMS_addSprite(bulletBehavior[0], bulletBehavior[1], BULLET_TILE);
-  }
-}
-
-void moveEnemy(void) {
-  if(enemyBehavior[2] == 1) {
-    enemyBehavior[2] = 0;
-    enemyBehavior[1] = INIT_E_POSY;
-    enemyBehavior[0] = randEnemyXPos();
-  } else {
-    ++enemyBehavior[1];
-  }
+//====================================
+// Drawing
+//====================================
+void drawPlayer(void) {
+  SMS_addSprite(playerPosition, INIT_P_POSY, SHIP_TILE);
 }
 
 void drawEnemy(void) {
@@ -202,10 +277,27 @@ void drawEnemy(void) {
   }
 }
 
+void drawBullet(void) {
+  if(bulletBehavior[2] == 0) {
+    SMS_addSprite(bulletBehavior[0], bulletBehavior[1], BULLET_TILE);
+  }
+}
+
+//====================================
+// Scenes
+//====================================
+void gameOverScene(void) {
+  gameState = ST_TITLE;
+  titleScene();
+}
+
 void mainScene(void) {
   playerPosition = INIT_P_POSX;
   enemyBehavior[2] = 1;
   bulletBehavior[2] = 1;
+  playerLives = START_LIVES;
+  score = 0;
+  level = 1;
   loadMainAssets();
   updateScore(score);
   updateLevel(level);
@@ -215,8 +307,7 @@ void mainScene(void) {
     movePlayer();
     moveEnemy();
     moveBullet();
-    checkEnemyOverlapsScreeen();
-    checkBulletCollsion();
+    checkBehavior();
     SMS_initSprites();
     drawPlayer();
     drawEnemy();
@@ -226,16 +317,7 @@ void mainScene(void) {
     SMS_copySpritestoSAT();
   }
   SMS_displayOff();
-  // game over
-}
-
-// GG display 160 x 144
-void loadTitleAssets(void) {
-  SMS_loadPSGaidencompressedTiles(title_scene__tiles__psgcompr, BG_TILES);
-  SMS_loadSTMcompressedTileMapArea(GG_X, GG_Y, title_scene__tilemap__stmcompr, GG_AREA);
-  GG_loadBGPalette(default__palette__bin);
-  SMS_loadPSGaidencompressedTiles(press_start__tiles__psgcompr, PS_TILES);
-  GG_setBGPaletteColor(4, RGB(3,3,3));
+  gameOverScene();
 }
 
 void titleScene(void) {
@@ -263,8 +345,5 @@ void titleScene(void) {
 
 void main(void) {
   gameState = ST_TITLE;
-  playerLives = START_LIVES;
-  score = 0;
-  level = 1;
   titleScene();
 }
---
+++ main.h
@@ -0,0 +1,9 @@
+#define ST_TITLE 0
+#define ST_MAIN 1
+#define ST_GAMEOVER 2
+
+void gameOverScene(void);
+
+void mainScene(void);
+
+void titleScene(void);
